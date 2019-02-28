88da264fbad53d5ab16f739f31a4569dc8acafae
yang <andresribeiro1996@gmail.com>
2019-02-28 00:07:42 +0000

    cenas
    cenas descriptio

---
+++ nextjs/pages/about.js
@@ -0,0 +1,8 @@
+
+import Layout from "../comps/MyLayout";
+
+export default () => (
+    <Layout>
+        <h1>Hellow About</h1>
+    </Layout>
+)
---
+++ nextjs/pages/index.js
@@ -0,0 +1,14 @@
+// This is the Link API
+import Link from 'next/link'
+import Header from '../comps/MyLayout'
+import Layout from '../comps/MyLayout';
+
+const Index = () => (
+  <div>
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+  </div>
+)
+
+export default Index
